de722d4b43df84de05618bbd7254b4bb3aa0f0a5
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-01-19 12:21:17 +0700

    Restructuring Folder & Add eslint (not configured)

---
+++ src/server/api/index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Todo = require('../models/Todo');
+const app = express();
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/jobQuestAutumn2016');
+
+app.get('/', (req, res) => {
+  Todo.find((err, todos) => {
+    res.send(todos);
+  });
+});
+
+app.post('/', (req, res) => {
+  var todo = new Todo(req.body);
+  todo.save(() => {
+    res.send({message:'Success'});
+  })
+});
+
+app.delete('/:id', (req,res) => {
+  Todo.remove({_id:req.params.id}, () => {
+    res.send({message: 'deleted'});
+  });
+})
+
+app.post('/:id/toggle', (req,res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    Todo.findByIdAndUpdate(req.params.id, {done: !todo.done}, () => {
+      res.send({message: 'success'});
+    })
+  })
+})
+
+app.listen(3001, () => {
+  console.log('[API] listening to port '+ 3001);
+})
--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname , '../dist')));
+app.use(express.static(path.resolve(__dirname , '../../dist')));
 app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
 app.listen(3000, function() { 
